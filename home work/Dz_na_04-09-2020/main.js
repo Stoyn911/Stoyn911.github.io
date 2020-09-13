c855f5b9cf53e6411a7a42686aaa853248226fe9
Stoyn <tartaky@gmail.com>
2020-09-14 00:32:11 +0300

    code_xxxx

---
+++ home work/Dz_na_04-09-2020/main.js
@@ -0,0 +1,45 @@
+let move = 'off';
+let door = 'closed';
+let code = 'xxxx';
+if (move === 'off' && door === 'closed') alert('code')
+console.log('code')
+
+/*
+const cat = {
+   name: "kot"
+   breed: "Siamese";
+   female: "yes";
+   male: "not";
+   a type: "predatory";
+   paws: "four";
+   ears: "2";
+   nail: "20";
+   teeth: "36";
+   tail length: "50";
+   coat size: "30";
+   castrated: "not";
+   type: "sluggish";
+   predator: "yes";
+   affectionate: "yes";
+   beautiful: "yes";
+   weight: "7";
+   age: "21";
+   tone: "100";
+   appetite: "100";
+   health: "100";
+      
+      short information:{
+         heirs: "not";
+         document: "yes";
+         marks: "not";
+         restroom: "yes";
+         bully: "yes";
+         purrs: "yes";
+         evil: "yes";
+         kind: "yes";
+         hungry: "yes";
+      }
+
+   cat pussy: "unrepeatable"; 
+}
+*/
